dfd7a6d755055ee44d664f49ce9b1bb84f3809ff
Charles <GUITARPLRC@YAHOO.COM>
2016-12-22 15:49:57 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,70 @@
+var Search = React.createClass({
+	getInitialState: function() {
+		return { title: "" };
+	},
+	
+	componentDidMount: function() {
+		$.get('https://api.themoviedb.org/3/search/movie?api_key=c4caddf3d2f1e3a21633c2611179f2e4&query=' + this.state.title, function(data) {
+			console.log(data);
+		});
+	},
+	
+	handleSubmit: function(e) {
+		e.preventDefault();
+		var search = this.refs.search;
+		// add search card
+		search.value = "";
+	},
+	
+	render: function() {
+		return (
+			<form onSubmit={this.handleSubmit}>
+				<input ref="search" />
+				<button>Search</button>
+			</form>
+		);
+	}
+});
+
+
+var MoviesInTheatres = React.createClass({
+	
+	getInitialState: function() {
+		return {};
+	},
+	
+	componentDidMount: function() {
+		var component = this;
+		$.get('https://api.themoviedb.org/', function(data) {
+			component.setState(data);
+			console.log(data);
+		});
+	},
+	
+	render: function() {
+		return (
+			<div>
+				<h3>In Theatres Now</h3>
+			</div>
+		);
+	}
+});
+
+
+var Main = React.createClass({
+	addCard: function(searchTitle) {
+		this.setState();
+	},
+	render: function() {
+		return (
+			<div id='main'>
+				<h1>Movie Search</h1>
+				<Search addCard={this.addCard}/>
+				<MoviesInTheatres />
+			</div>
+		);
+	}
+});
+
+
+ReactDOM.render(<Main />, document.getElementById('app'));
